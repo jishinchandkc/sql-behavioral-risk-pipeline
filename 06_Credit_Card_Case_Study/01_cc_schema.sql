bd1f7a44863951e854832bd331916554d6b45998
Jishin Chand K C <jishin.chand@gmail.com>
2026-04-01 15:39:35 +0530

    Delete 06_Credit_Card_Case_Study directory

--- 06_Credit_Card_Case_Study/01_cc_schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE credit_card_transcations (
-    transaction_id INT PRIMARY KEY,
-    city VARCHAR(100),
-    transaction_date DATE,
-    card_type VARCHAR(20),
-    exp_type VARCHAR(50),
-    gender CHAR(1),
-    amount INT
-);
